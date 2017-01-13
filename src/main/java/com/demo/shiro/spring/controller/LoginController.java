21b20348d759e912bc60e9baf3a1e4a72cd4725c
kuang135 <490543013@qq.com>
2017-01-13 16:52:02 +0800

    WEB

--- src/main/java/com/demo/shiro/spring/controller/LoginController.java
+++ src/main/java/com/demo/shiro/spring/controller/LoginController.java
@@ -1,8 +1,25 @@
 package com.demo.shiro.spring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @Author yaowk
  * @Date 2017/1/13 16:17
  */
+@Controller
 public class LoginController {
+
+    @RequestMapping("login.do")
+    public String login() {
+        System.out.println("login");
+        return "login";
+    }
+
+
+    @RequestMapping("logout.do")
+    public String logout() {
+        System.out.println("logout");
+        return "login";
+    }
 }
--- src/main/java/com/demo/shiro/spring/realm/UserRealm.java
+++ src/main/java/com/demo/shiro/spring/realm/UserRealm.java
@@ -1,8 +1,24 @@
 package com.demo.shiro.spring.realm;
 
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
 /**
  * @Author yaowk
  * @Date 2017/1/13 16:17
  */
-public class UserRealm {
+public class UserRealm extends AuthorizingRealm {
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        return null;
+    }
 }
